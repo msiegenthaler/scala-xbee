ec6fec34ddf3821df945762aac902c95b2709d9c
msiegenthaler <msiegenthaler@gmx.net>
2010-10-24 14:26:44 +0200

    changed build system to use maven to download libs

--- project/build/ScalaXbee.scala
+++ project/build/ScalaXbee.scala
@@ -3,11 +3,21 @@ import sbt._
 class ScalaXbeeProject(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins {      
   val continuations = compilerPlugin("org.scala-lang.plugins" % "continuations" % "2.8.0")
   override def compileOptions = CompileOption("-P:continuations:enable") :: CompileOption("-unchecked") :: super.compileOptions.toList
-  
-  val logbackcore = "ch.qos.logback" % "logback-core" % "0.9.24"
-  val logbackclassic = "ch.qos.logback" % "logback-classic" % "0.9.24"
-  
-  override def testClasspath = super.testClasspath +++ ("lib-test" / "scalabase_2.8.0-2.1.0-SNAPSHOT-test.jar") 
-  val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test"
-  val toolsSnapshot = ScalaToolsSnapshots
+
+  override def packageSrcJar= defaultJarPath("-sources.jar")
+  val sourceArtifact = Artifact.sources(artifactID)
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
+
+  val inventsoftReleases = "Inventsoft Release Repository" at "http://mavenrepo.inventsoft.ch/repo"
+  val inventsoftSnapshots = "Inventsoft Snapshot Repository" at "http://mavenrepo.inventsoft.ch/snapshot-repo"
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = Resolver.sftp("Inventsoft Publish", "foxtrot.inventsoft.ch", "/inventsoft/dev/mavenrepo/snapshot-repo")
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+
+  val scalabase = "ch.inventsoft" % "scalabase_2.8.0" % "2.1.0-SNAPSHOT"
+  val rxtx = "rxtx" % "rxtx" % "20101024"
+
+  val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
+  val logbackcore = "ch.qos.logback" % "logback-core" % "0.9.24" % "test"
+  val logbackclassic = "ch.qos.logback" % "logback-classic" % "0.9.24" % "test"
 }
